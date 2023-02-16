2431c14f307f35fecdbabc542026bdd1938f0d4e
25asheng <25asheng@castilleja.org>
2023-02-16 13:06:48 -0800

    squares

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import processing.core.PApplet;
+public class Main extends PApplet{
+    public static void main(String[] args){
+        PApplet.main("Main");
     }
+
+    public void settings(){
+        size(600,600);
+    }
+
+    public void setup(){}
+
+    public void draw(){
+        rect(50,50,100,100);
+
+    }
+
 }
